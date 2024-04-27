8296e7281d6e099495fe7f77b9cd9ee14311c0d0
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-04-27 15:23:52 -0700

    Move text above image

--- src/pages/about-page/Sections.js
+++ src/pages/about-page/Sections.js
@@ -2,14 +2,15 @@ import { Box, Typography } from "@mui/material"
 
 export function SectionOne() {
   return (
-    <Box sx={{ height: '825px', px: '10vw', py: '150px' }}>
+    <Box sx={{ height: '825px', px: '10vw', py: '150px', zIndex: 0}}>
       <Box 
         bgcolor='silver.dark'
         display='flex'
         justifyContent='center'
         sx={{
           width: '100%',
-          height: '100%'
+          height: '100%',
+          zIndex: 1
       }}>
         <Box
           sx={{
@@ -25,14 +26,15 @@ export function SectionOne() {
               height: 700,
               pt: 10,
               pl: 10,
-              pr: 7
+              pr: 7,
+              zIndex: 2
           }}>
-            <Typography variant='body1'>
+            <Typography variant='body1' sx={{ zIndex: 4 }}>
               <p>Hello,<br/>My name is Allen!</p>
             </Typography>
-            <Typography variant='body2' color='silver.dark'>
+            <Typography variant='body2' color='silver.dark' zIndex='4'>
               <p>I'm a Bachelor of Computer Science, graduated from Cal Poly Pomona.</p>
-              <p>My area's of interest include back-end development, machine learning, and general problem solving.</p>
+              <p>My areas of interest include back-end development, machine learning, and general problem solving.</p>
               <p>With a detail oriented-focus, I enjoy creating simple but effective solutions to improve application performance, ease of maintenance, and user experience.</p>
             </Typography>
           </Box>
@@ -44,8 +46,29 @@ export function SectionOne() {
               position: 'absolute',
               top: 80,
               right: 0,
-              height: 600
+              height: 600,
+              zIndex: 3
           }} />
+          <Box
+            sx={{
+              position: 'absolute',
+              top: 125,
+              width: 350,
+              height: 700,
+              pt: 10,
+              pl: 10,
+              pr: 7,
+              zIndex: 4
+          }}>
+           <Typography variant='body1'>
+              <p>Hello,<br/>My name is Allen!</p>
+            </Typography>
+            <Typography variant='body2' color='silver.dark'>
+              <p>I'm a Bachelor of Computer Science, graduated from Cal Poly Pomona.</p>
+              <p>My areas of interest include back-end development, machine learning, and general problem solving.</p>
+              <p>With a detail oriented-focus, I enjoy creating simple but effective solutions to improve application performance, ease of maintenance, and user experience.</p>
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
@@ -68,6 +91,19 @@ export function SectionTwo() {
       }}>
         
       </Box>
+    </Box>
+  )
+}
+
+export function SectionThree() {
+  return (
+    <Box
+      bgcolor='melon.light' 
+      sx={{
+        height: '1000px',
+        my: '150px'
+    }}>
+
     </Box>
   )
 }
