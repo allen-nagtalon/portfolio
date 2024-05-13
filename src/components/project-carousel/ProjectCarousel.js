f6232774fba57c4e07823c19bc7e19ede14124f0
Allen Nagtalon <ant.nagtalon22@gmail.com>
2024-05-13 05:37:36 -0700

    Migrate projects array to the project section

--- src/components/project-carousel/ProjectCarousel.js
+++ src/components/project-carousel/ProjectCarousel.js
@@ -93,54 +93,10 @@ const ProjectCard = (props) => {
 }
 
 const ProjectCarousel = (props) => {
-  const projects = [
-    {
-      name: 'Wishlist Organizer',
-      description: 'An Angular and Spring full-stack application for users to create and share their wishlist of items for events.',
-      snippet: 'Wishlists ',
-      image: 'wishlist.jpg',
-      tech: [ 'Java', 'Typescript', 'HTML', 'CSS', 'MySQL', 'Maven', 'Spring Framework' ],
-      github: 'https://github.com/allen-nagtalon/wishlist-web-app',
-      wip: 'true'
-    },
-    {
-      name: 'Triple Triad Tracker',
-      description: 'A React and Django full-stack application for users to track their Triple Triad Card collection in the MMORPG "Final Fantasy XIV.',
-      snippet: 'Allows users to search through the complete collection of Triple Triad cards, view related stats and info, and mark cards that have been acquired in game.',
-      image: 'ffxiv_triple_triad.png',
-      tech: [ 'Python', 'Javascript', 'HTML', 'CSS', 'Django', 'React', 'JWT', 'Axios', 'Material-UI' ],
-      github: 'https://github.com/allen-nagtalon/triadtracker'
-    },
-    {
-      name: 'Flappy Bird Clone w/ NEAT Neural Network',
-      description: 'A Python recreation of the mobile game Flappy Bird with an implementation of a NEAT Neural Network to play the game.',
-      snippet: '',
-      image: 'neat_flappy.png',
-      tech: [ 'Python', 'PyGame', 'NEAT Neural Network' ],
-      github: 'https://github.com/allen-nagtalon/neat_flappy'
-    },
-    {
-      name: 'Sudoku Solver Mobile App',
-      description: 'A simple Android mobile application to read Sudoku puzzles using AR that can either be played out or solved.',
-      snippet: '',
-      image: 'sudoku_solver.jpg',
-      tech: [ 'Java', 'Kotlin', 'Android Studio', 'CameraX API', 'Google MLK', 'Gradle' ],
-      github: 'https://github.com/anthony-nagtalon/SudokuWizardApp'
-    },
-    {
-      name: 'Top-View RPG Game',
-      description: 'A top-view 2D game developed in Python using Pygame to learn the basic and intermediate features of the modules. Created following the online course for Pygame development via Clear Code.',
-      snippet: '',
-      image: 'top_view_rpg.png',
-      tech: [ 'Python', 'PyGame', 'Tiled' ],
-      github: 'https://github.com/anthony-nagtalon/zelda_like_game'
-    },
-  ]
-
   return (
     <Carousel indicators={false} navButtonsAlwaysVisible={true} autoPlay={false} duration={1000}>
       {
-        projects.map( (project, i) => <ProjectCard project={project} key={i} /> )
+        props.projects.map( (project, i) => <ProjectCard project={project} key={i} /> )
       }
     </Carousel>
   )
--- src/pages/main-page/ProjectsSection.js
+++ src/pages/main-page/ProjectsSection.js
@@ -2,6 +2,50 @@ import { Box, Typography } from '@mui/material'
 import ProjectCarousel from '../../components/project-carousel/ProjectCarousel.js'
 
 export function ProjectsSection() {
+  const projects = [
+    {
+      name: 'Wishlist Organizer',
+      description: 'An Angular and Spring full-stack application for users to create and share their wishlist of items for events.',
+      snippet: 'Wishlists ',
+      image: 'wishlist.jpg',
+      tech: [ 'Java', 'Typescript', 'HTML', 'CSS', 'MySQL', 'Maven', 'Spring Framework' ],
+      github: 'https://github.com/allen-nagtalon/wishlist-web-app',
+      wip: 'true'
+    },
+    {
+      name: 'Triple Triad Tracker',
+      description: 'A React and Django full-stack application for users to track their Triple Triad Card collection in the MMORPG "Final Fantasy XIV.',
+      snippet: 'Allows users to search through the complete collection of Triple Triad cards, view related stats and info, and mark cards that have been acquired in game.',
+      image: 'ffxiv_triple_triad.png',
+      tech: [ 'Python', 'Javascript', 'HTML', 'CSS', 'Django', 'React', 'JWT', 'Axios', 'Material-UI' ],
+      github: 'https://github.com/allen-nagtalon/triadtracker'
+    },
+    {
+      name: 'Flappy Bird Clone w/ NEAT Neural Network',
+      description: 'A Python recreation of the mobile game Flappy Bird with an implementation of a NEAT Neural Network to play the game.',
+      snippet: '',
+      image: 'neat_flappy.png',
+      tech: [ 'Python', 'PyGame', 'NEAT Neural Network' ],
+      github: 'https://github.com/allen-nagtalon/neat_flappy'
+    },
+    {
+      name: 'Sudoku Solver Mobile App',
+      description: 'A simple Android mobile application to read Sudoku puzzles using AR that can either be played out or solved.',
+      snippet: '',
+      image: 'sudoku_solver.jpg',
+      tech: [ 'Java', 'Kotlin', 'Android Studio', 'CameraX API', 'Google MLK', 'Gradle' ],
+      github: 'https://github.com/anthony-nagtalon/SudokuWizardApp'
+    },
+    {
+      name: 'Top-View RPG Game',
+      description: 'A top-view 2D game developed in Python using Pygame to learn the basic and intermediate features of the modules. Created following the online course for Pygame development via Clear Code.',
+      snippet: '',
+      image: 'top_view_rpg.png',
+      tech: [ 'Python', 'PyGame', 'Tiled' ],
+      github: 'https://github.com/anthony-nagtalon/zelda_like_game'
+    },
+  ]
+
   return (
     <Box
       bgcolor='silver.light'
@@ -11,7 +55,7 @@ export function ProjectsSection() {
         px: '5vw',
         py: '150px'
     }}>
-      <ProjectCarousel />
+      <ProjectCarousel projects={projects} />
     </Box>
   )
 }
